95e877039c66ab00520af22fee8caa5263f23b09
Mikkel <cph-mh1088@cphbusiness.dk>
2024-01-20 16:33:30 +0100

    msg

--- src/components/EventDetails.jsx
+++ src/components/EventDetails.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
 import eventFacade from "/src/facade/eventFacade.js";
 
 const EventDetails = () => {
--- src/components/NoMatch.jsx
+++ src/components/NoMatch.jsx
@@ -2,7 +2,7 @@ const NoMatch = () => {
   return (
     <div>
       <h1>404 - Not Found</h1>
-      <h3>Denne side side findes ikke. Gå tilbage til index siden</h3>
+      <h3>Denne side findes ikke. Gå tilbage til index siden</h3>
     </div>
   );
 };
